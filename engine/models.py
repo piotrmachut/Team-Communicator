29d4c82ff42bcd745d927278c5e320fa0251e3d1
piotrmachut <piotr.machut@gmail.com>
2021-03-06 18:46:57 +0100

    Add docstrings to models.

--- engine/models.py
+++ engine/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Team(models.Model):
+    """Group of users messaging together."""
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_teams')
@@ -10,6 +11,7 @@ class Team(models.Model):
 
 
 class BaseMessage(models.Model):
+    """Abstract model for all channels messages."""
     sender = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
@@ -19,12 +21,15 @@ class BaseMessage(models.Model):
 
 
 class MainChannelMessage(BaseMessage):
+    """Messages visible to all users."""
     pass
 
 
 class TeamMessage(BaseMessage):
+    """Messages visible for members of selected team (Team model)."""
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
 
 class PrivateMessage(BaseMessage):
+    """Messages between two users."""
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
